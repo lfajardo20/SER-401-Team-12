f5de8ae55a1dea8bd9d51b63a1cf9031d2f9579f
Chris <cdcody@asu.edu>
2018-11-07 21:24:54 -0700

    Can go to calendar page: has nothing but a text field

--- app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
@@ -1,13 +1,27 @@
 package com.capstoneproject.arrivalnotification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startCalendar(View view) {
+        Intent intent = new Intent(this, Calendar.class);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String message = textView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
