2e88b3b40325515fe8a862e971db7a544caba85e
Chris <cdcody@asu.edu>
2018-11-09 21:33:21 -0700

    More experimentation with nav activities vs fragments; little real change

---
+++ app/src/main/java/com/capstoneproject/arrivalnotification/CalendarActivity.java
@@ -0,0 +1,44 @@
+package com.capstoneproject.arrivalnotification;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_barcode:
+                    mTextMessage.setText("Barcode");
+                    return true;
+                case R.id.navigation_calendar:
+                    mTextMessage.setText("Calendar");
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
@@ -2,20 +2,40 @@ package com.capstoneproject.arrivalnotification;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedListener = (MenuItem item) -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_barcode:
+                case R.id.navigation_calendar:
+                case R.id.navigation_notifications:
+                    return true;
+                default:
+                    return false;
+            }
+        };
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(selectedListener);
     }
 
     public void startCalendar(View view) {
-        Intent intent = new Intent(this, Calendar.class);
+        Intent intent = new Intent(this, CalendarActivity.class);
         //TextView textView = (TextView) findViewById(R.id.textView);
         // String message = textView.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, "hi");
