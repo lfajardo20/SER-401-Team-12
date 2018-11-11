7c7de53ef665f68d3798c5d0677d668788da208d
hongqi zhang <35391541+hzhan193@users.noreply.github.com>
2018-11-10 23:48:43 -0700

    Update app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
    add textView and some function, but not finished

--- app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/MainActivity.java
@@ -3,6 +3,7 @@ package com.capstoneproject.arrivalnotification;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
@@ -13,6 +14,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
     //private CameraCaptureSession mSession;
     private CameraManager cameraManager;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
+    /*
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }*/
+
     CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback(){
         @Override
         public void onOpened(@NonNull CameraDevice camera){
@@ -48,6 +59,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        textureView = (TextureView) findViewById(R.id.texture);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(textureListener);
+        takePictureButton = (Button) findViewById(R.id.btn_takepicture);
+        assert takePictureButton != null;
+        */
+        /*
+        TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                //open your camera here
+                openCamera();
+            }
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+                // Transform you image captured size according to the surface width and height
+            }
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            }
+        };*/
         Button btn_camera = (Button) findViewById(R.id.btn_camera);
         btn_camera.setOnClickListener(new View.OnClickListener(){
             @Override
