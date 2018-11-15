56cc075cc018804201b0a92dbf1a883d16e6bb76
Chris <cdcody@asu.edu>
2018-11-14 20:53:52 -0700

    Started android recyclerview tutorial

--- app/src/main/java/com/capstoneproject/arrivalnotification/NotificationActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/NotificationActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 
 public class NotificationActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +35,27 @@ public class NotificationActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
         bottomNavigation.setOnNavigationItemSelectedListener(selectedListener);
+
+        recyclerView = findViewById(R.id.notification_recycler);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] data = {"hi", "hello",};
+        adapter = new NotificationAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 
     public void startBarcode(MenuItem menu) {
         Intent intent = new Intent(this, MainActivity.class);
         //TextView textView = (TextView) findViewById(R.id.textView);
         // String message = textView.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, "hi");
         startActivity(intent);
     }
 
     public void startCalendar(MenuItem item) {
         Intent intent = new Intent(this, CalendarActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "hello notifications");
         startActivity(intent);
     }
 
