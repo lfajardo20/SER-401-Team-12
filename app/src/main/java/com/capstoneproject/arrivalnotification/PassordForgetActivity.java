be31cd557e7765e3824704a6a0d8bfa6a127c233
hongqi zhang <35391541+hzhan193@users.noreply.github.com>
2018-11-16 23:54:35 -0700

    US13 Task46<provide forget password GUI>
    provide forget password GUI
    add an email text
    add a button to send email

---
+++ app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.arrivalnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassordForgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passord_forget);
+    }
+}
