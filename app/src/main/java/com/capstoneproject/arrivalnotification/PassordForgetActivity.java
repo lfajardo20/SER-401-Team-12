2bbee06115c02fd323f9d6f8c12f07808ab0803e
hongqi zhang <35391541+hzhan193@users.noreply.github.com>
2018-11-17 18:08:47 -0700

    US13Task53<forget password>
    add basic code to the PasswordForgetActivity.java
    fix the id of textview

--- app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
@@ -2,12 +2,27 @@ package com.capstoneproject.arrivalnotification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PassordForgetActivity extends AppCompatActivity {
 
+    private Button btn_send_email;
+    private TextView txt_email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passord_forget);
+
+        btn_send_email = (Button) findViewById(R.id.btn_sendEmail);
+        txt_email = (TextView) findViewById(R.id.etxt_email);
+
+        btn_send_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get request to server, gennerate token and send email to user
+            }
+        });
     }
 }
