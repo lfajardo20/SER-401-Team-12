26ec2a10e2dae6d40e871727ebc0880f8806680d
hongqi zhang <35391541+hzhan193@users.noreply.github.com>
2018-11-21 23:47:37 -0700

    US13Task53<research code example>
    a samle coode connecting to node js server, not yet test

--- app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
@@ -17,11 +17,41 @@ public class PassordForgetActivity extends AppCompatActivity {
 
         btn_send_email = (Button) findViewById(R.id.btn_sendEmail);
         txt_email = (TextView) findViewById(R.id.etxt_email);
+/*
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://localhost:8008/postdata"; // your URL
+
+        queue.start();*/
 
         btn_send_email.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 //get request to server, gennerate token and send email to user
+                /*
+                HashMap<String, String> params = new HashMap<String,String>();
+                params.put("data", DataField.getText().toString()); // the entered data as the JSON body.
+
+                JsonObjectRequest jsObjRequest = new
+                        JsonObjectRequest(Request.Method.POST,
+                        url,
+                        new JSONObject(params),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    DisplayText.setText(response.getString("message"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        DisplayText.setText("That didn't work!");
+                    }
+                });
+                queue.add(jsObjRequest);*/
             }
         });
     }
