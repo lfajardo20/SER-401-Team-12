12676c6f0df51ff728880c5c9295c275f837ae40
hongqi zhang <35391541+hzhan193@users.noreply.github.com>
2018-11-23 23:23:40 -0700

    US13Task53<connect to server>
    try to connect to server with http request, but function not fully implemented
    add Internet permission

--- app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/PassordForgetActivity.java
@@ -6,6 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class PassordForgetActivity extends AppCompatActivity {
 
     private Button btn_send_email;
@@ -52,7 +61,43 @@ public class PassordForgetActivity extends AppCompatActivity {
                     }
                 });
                 queue.add(jsObjRequest);*/
+                String urlString = "localhost:8008/";
+                try{
+                    JSONObject jsonObject = getJSONObjectFromURL(urlString);
+                    //
+                    // Parse your json here
+                    //
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        URL url = new URL(urlString);
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(10000 /* milliseconds */ );
+        urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+        urlConnection.setDoOutput(true);
+        urlConnection.connect();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+
+        String jsonString = sb.toString();
+        System.out.println("JSON: " + jsonString);
+
+        return new JSONObject(jsonString);
     }
 }
