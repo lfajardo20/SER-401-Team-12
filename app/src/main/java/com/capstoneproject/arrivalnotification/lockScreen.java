5cef845d24d47dabf2670b8c07a0876e5b0c8c12
Mechris6 <mechris6@asu.edu>
2018-11-27 11:10:11 -0700

    Added lockscreen to doctor's view

--- app/src/main/java/com/capstoneproject/arrivalnotification/lockScreen.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/lockScreen.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import a
 import android.widget.Toast;
 import android.view.inputmethod.InputMethodManager;
 
@@ -28,10 +27,5 @@ public class lockScreen extends AppCompatActivity
 
         TextView pin = this.findViewById(R.id.pinInput);
         pin.requestFocus();
-
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-
-        if(pin.measureText() )
     }
 }
