00d098c1e945f2d420f910a615fc7f9f67d66d2e
lfajardo20 <lfajardo@asu.edu>
2018-11-30 23:34:27 -0700

    Currently working on getting the connection ready

--- app/src/main/java/com/capstoneproject/arrivalnotification/login.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/login.java
@@ -11,6 +11,15 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class login extends AppCompatActivity {
 
     private Button btn_forget_pwd;
@@ -21,14 +30,24 @@ public class login extends AppCompatActivity {
         EditText pwd = (EditText) findViewById(R.id.passwordEdit);
         Intent changeActivity;
         JSONObject obj;
+        URL url = new URL("https://awk4q8rl4b.execute-api.us-west-1.amazonaws.com/test");
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }
         String json = "{"
                         + "\"userName\":" + "\"" + usr.getText().toString().trim() + "\","
                         + "\"password\":" + "\"" + pwd.getText().toString().trim() + "\""
                         +"}";
         try {
             obj = new JSONObject(json);
-            Toast.makeText(this,"json string is: usrname = " + obj.getString("userName") + ",pwd = " + obj.getString("password"),
-                    Toast.LENGTH_LONG).show();
+
+
+//            Toast.makeText(this,"json string is: usrname = " + obj.getString("userName") + ",pwd = " + obj.getString("password"),
+//                    Toast.LENGTH_LONG).show();
         }catch (JSONException e){
             e.printStackTrace();
         }
