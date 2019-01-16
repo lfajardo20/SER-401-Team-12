b5a9d2ccafe67f29d0378f25c4699d0e7f22b96b
Chris <cdcody@asu.edu>
2019-01-16 16:28:16 -0700

    Added sample code for barcode scanner

--- src/scanner/scanner.js
+++ src/scanner/scanner.js
@@ -1,20 +1,118 @@
 import React, { Component } from "react";
-import { Text, Button, View } from "react-native";
-import { RNCamera } from "react-native-camera";
+import { Alert, View, Text, Vibration, StyleSheet } from "react-native";
+import { Camera, BarCodeScanner, Permissions } from "expo";
 
-export default class BarcodeScanner extends Component {
-  state = {
-    camera: {
-      type: RNCamera.Constants.Type.back,
-      flashMode: RNCamera.Constants.FlashMode.auto,
-      barcodeFinderVisible: true,
-    },
-  };
-  render() {
+export default class Scanner extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onBarCodeRead = this.onBarCodeRead.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
+    this.scannedCode = null;
+
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back,
+    };
+  }
+
+  async UNSAFE_componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    await this.setState({ hasCameraPermission: status === "granted" });
+    await this.resetScanner();
+  }
+
+  renderAlert(title, message) {
+    Alert.alert(
+      title,
+      message,
+      [{ text: "OK", onPress: () => this.resetScanner() }],
+      { cancelable: true }
+    );
+  }
+
+  onBarCodeRead({ type, data }) {
+    if (
+      (type === this.state.scannedItem.type &&
+        data === this.state.scannedItem.data) ||
+      data === null
+    ) {
+      return;
+    }
+
+    Vibration.vibrate();
+    this.setState({ scannedItem: { data, type } });
+
+    if (type.startsWith("org.gs1.EAN")) {
+      // Process EAN code
+      this.resetScanner();
+      this.props.navigation.navigate("YOUR_NEXT_SCREEN", { ean: data });
+    } else if (type.startsWith("org.iso.QRCode")) {
+      // Process QRCode
+      this.resetScanner();
+    } else {
+      this.renderAlert("This barcode is not supported.", `${type} : ${data}`);
+    }
+  }
+
+  renderMessage() {
+    if (this.state.scannedItem && this.state.scannedItem.type) {
+      const { type, data } = this.state.scannedItem;
+      return (
+        <Text style={styles.scanScreenMessage}>
+          {`Scanned \n ${type} \n ${data}`}
+        </Text>
+      );
+    }
     return (
-      <View>
-        stuff<div>nesting to make sure formatting works</div>
+      <Text style={styles.scanScreenMessage}>Focus the barcode to scan.</Text>
+    );
+  }
+
+  resetScanner() {
+    this.scannedCode = null;
+    this.setState({
+      scannedItem: {
+        type: null,
+        data: null,
+      },
+    });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <BarCodeScanner
+            onBarCodeScanned={this.onBarCodeRead}
+            style={StyleSheet.absoluteFill}
+          />
+          {this.renderMessage()}
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: "#fff",
+  },
+  scanScreenMessage: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
