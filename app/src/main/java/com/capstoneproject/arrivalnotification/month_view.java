d2cc5008f2c479c042b7916f595c65e9fa9d33ab
Leirbag Gonzalez <lfajardo@asu.edu>
2019-01-22 20:31:18 -0700

    Added a functionality that will select the highlighted date from the calendar.

--- app/src/main/java/com/capstoneproject/arrivalnotification/month_view.java
+++ app/src/main/java/com/capstoneproject/arrivalnotification/month_view.java
@@ -4,27 +4,42 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.capstoneproject.arrivalnotification.Notification.NotificationActivity;
 import com.capstoneproject.arrivalnotification.calendar.CalendarActivity;
 import com.capstoneproject.arrivalnotification.calendar.CalendarDayAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class month_view extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    public String date;
+    public CalendarView dateHighlighted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_view);
+
+        Button dateSelected = findViewById(R.id.daySelected_b);
+        dateSelected.setOnClickListener(this::onClick);
+        dateHighlighted = findViewById(R.id.calendarView);
 
         BottomNavigationView.OnNavigationItemSelectedListener selectedListener = (MenuItem item) -> {
             switch (item.getItemId()) {
@@ -41,10 +56,26 @@ public class month_view extends AppCompatActivity {
             }
         };
 
+        //Gets the highlighted date and puts it into date var.
+        dateHighlighted.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                date = month + "/" + (day + 1) + "/" + year;
+            }
+        });
+
         BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
         bottomNavigation.setOnNavigationItemSelectedListener(selectedListener);
 
         createNotificationChannel();
+    }
+
+    public void onClick(View view){
+        if (view.getId() == R.id.daySelected_b){
+            Toast.makeText(this, "Going to date " + date + ".", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, CalendarActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void startNotifications(MenuItem item) {
@@ -53,6 +84,7 @@ public class month_view extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //Starts the day view
     public void startCalendar(MenuItem menu) {
         Intent intent = new Intent(this, CalendarActivity.class);
         //TextView textView = (TextView) findViewById(R.id.textView);
@@ -61,6 +93,7 @@ public class month_view extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //Starts the month view
     public void startMonthView(MenuItem menu){
         Intent intent = new Intent(this, month_view.class);
         startActivity(intent);
