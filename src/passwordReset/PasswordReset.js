f209a54b574608fbd9bcd112964ecf27492a8cee
hongqi Zhang <hzhan193@asu.edu>
2019-02-02 22:39:02 -0700

    Update PasswordReset.js
    update view

--- src/passwordReset/PasswordReset.js
+++ src/passwordReset/PasswordReset.js
@@ -1,20 +1,55 @@
-import React, { Component } from "react";
-import { Text, Button, View } from "react-native";
-import { RNCamera } from "react-native-camera";
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput,
+    Image,
+    Animated
+} from 'react-native';
+class ForgotPasswordController extends Component {
+    /**
+     * Default props
+     */
+    static defaultProps = {
+        backgroundColor :"white",
+        titleText:"Forgot Password",
+        submitText:"Send",
+        placeHolderText:"Email Address"
+    };
 
-export default class BarcodeScanner extends Component {
-  state = {
-    camera: {
-      type: RNCamera.Constants.Type.back,
-      flashMode: RNCamera.Constants.FlashMode.auto,
-      barcodeFinderVisible: true,
-    },
-  };
-  render() {
-    return (
-      <View>
-        stuff<div>nesting to make sure formatting works</div>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+           email:""
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={[styles.bottomView,{backgroundColor:this.props.backgroundColor}]}>
+                    <TouchableOpacity style={styles.btnClose} activeOpacity={0.6} onPress={() => this.btnClosePress()}>
+                        <Image source={Images.close}/>
+                    </TouchableOpacity>
+                    <Text style={styles.textHeader}>{this.props.titleText}</Text>
+                    <View style={styles.starView}>
+                        <View style={styles.inputView}>
+                            <TextInput style={styles.inputText} placeholder={this.props.placeHolderText}
+                            multiline={false} placeholderTextColor={'#3c3c3c'} autoCapitalize={'none'} keyboardType={'email-address'} autoCorrect={false} underlineColorAndroid={'transparent'} onChangeText={(email) => this.setState({email})} value={this.state.email}></TextInput>
+                        </View>
+                        <TouchableOpacity style={styles.btnCancel} activeOpacity={0.6} onPress={() => this.btnSubmitPress()}>
+                            <Text style={styles.textCancel} numberOfLines={1}>
+                            {this.props.submitText}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
+
+export default ForgotPasswordController
