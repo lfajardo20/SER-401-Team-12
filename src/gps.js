296c33134ed5bfc9399b75720125bcaf3dab9585
mechris6 <mechris6@asu.edu>
2019-02-14 00:13:55 -0700

    Added function to check for GPS and netowrk location status

--- src/gps.js
+++ src/gps.js
@@ -24,7 +24,6 @@ export default class gps extends React.Component
         Long2: -112.03986336,
     }
   
-   
     EASTPACUPreop = {
         Lat: 33.4797482,
         Long: -112.0411164,
@@ -36,9 +35,11 @@ export default class gps extends React.Component
   state = {
     location: null,
     hasLocationPermission: null,
+    GPSenabled: null,
+    NetworkEnabled: null,
   };
 
-  componentWillMount() 
+  UNSAFE_componentWillMount() 
   {
       this.getLocationAsync();
   }
@@ -50,45 +51,60 @@ export default class gps extends React.Component
         this.hasLocationPermission = "Location request denied";
     }
 
-    let location = await Location.getCurrentPositionAsync({});
+    //Used to get network status
+    let locationStatus = await Location.getProviderStatusAsync({});
+    this.state.GPSenabled = locationStatus.gpsAvailable;
+    this.state.NetworkEnabled = locationStatus.networkAvailable;
+
+    let location = await Location.getCurrentPositionAsync({}); 
     this.setState({ location });
   };
 
-  render() {
+  render() 
+  {
     let text = 'Awaiting location';
+    
     if (this.state.hasLocationPermission) 
     {
       text = this.state.hasLocationPermission;
     } 
-    else if (this.state.location) 
+    else
     {
-      //Long list of comparing lat and long to get current location
-      //Moving to a separate function will make it prettier, so I should do that.      
-      if(this.state.location.coords.latitude >= this.MainPREOP.Lat2 && this.state.location.coords.latitude <= this.MainPREOP.Lat)
-      {
-          if(this.state.location.coords.longitude >= this.MainPREOP.Long2 && this.state.location.coords.longitude <= this.MainPREOP.Long)
+        //Need both gps and network to get a more accurate location
+        if(this.state.GPSenabled && this.state.NetworkEnabled)
+        {
+          //Long list of comparing lat and long to get current location
+          //Moving to a separate function will make it prettier, so I should do that.      
+          if(this.state.location.coords.latitude >= this.MainPREOP.Lat2 && this.state.location.coords.latitude <= this.MainPREOP.Lat)
           {
-            text = "Current Location is Main PREOP.";
+              if(this.state.location.coords.longitude >= this.MainPREOP.Long2 && this.state.location.coords.longitude <= this.MainPREOP.Long)
+              {
+                text = "Current Location is Main PREOP.";
+              }
           }
-      }
-      else if(this.state.location.coords.latitude >= this.EASTPreop.Lat2 && this.state.location.coords.latitude <= this.EASTPreop.Lat)
-      {
-          if(this.state.location.coords.longitude >= this.EASTPreop.Long2 && this.state.location.coords.longitude <= this.EASTPreop.Long)
+          else if(this.state.location.coords.latitude >= this.EASTPreop.Lat2 && this.state.location.coords.latitude <= this.EASTPreop.Lat)
           {
-            text = "Current Location is East PREOP.";
+              if(this.state.location.coords.longitude >= this.EASTPreop.Long2 && this.state.location.coords.longitude <= this.EASTPreop.Long)
+              {
+                text = "Current Location is East PREOP.";
+              }
           }
-      }
-       else if(this.state.location.coords.latitude >= this.EASTPACUPreop.Lat2 && this.state.location.coords.latitude <= this.EASTPACUPreop.Lat)
-      {
-          if(this.state.location.coords.longitude >= this.EASTPACUPreop.Long2 && this.state.location.coords.longitude <= this.EASTPACUPreop.Long)
+          else if(this.state.location.coords.latitude >= this.EASTPACUPreop.Lat2 && this.state.location.coords.latitude <= this.EASTPACUPreop.Lat)
           {
-            text = "Current Location is East PACU PREOP.";
+              if(this.state.location.coords.longitude >= this.EASTPACUPreop.Long2 && this.state.location.coords.longitude <= this.EASTPACUPreop.Long)
+              {
+                text = "Current Location is East PACU PREOP.";
+              }
           }
-      }
-      else
-      {          
-        text = "Error";
-      }
+          else
+          {          
+            text = "Error not in a valid location. \n" + this.state.location.coords.latitude + " ," + this.state.location.coords.longitude;
+          }
+        }
+        else
+        {
+            text = "GPS and Network are not enabled."
+        }
     }
     
     return (
