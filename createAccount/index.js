275c8d6811780c5602b6cc4061c3b57e21e7fc8c
Chris <cdcody@asu.edu>
2019-02-25 21:36:14 -0700

    Added server function for adding new account to database

---
+++ createAccount/index.js
@@ -0,0 +1,50 @@
+var mysql = require('mysql');//establish sql connection pool
+
+var pool  = mysql.createPool({
+    host: "184.103.137.162",
+    user: 'db',
+    password: 'password',
+    port: "3306",
+    database: 'user',
+  });
+  
+exports.handler = async (event, context, callback) => {
+    //parse event info
+    let {userName, passwordHash, userType, salt, phoneNumber} = event;
+  
+  var queryStr = "Insert into user.users" + 
+    "(userName, passwordHash, userType, salt, phoneNumber) " +
+    "Values (" + userName + ", " + passwordHash + ", " + userType + ", " + salt
+    + ", " + phoneNumber + ")";
+  
+    context.callbackWaitsForEmptyEventLoop = false;
+    return new Promise(function(resolve, reject)
+    {
+        try{
+            pool.getConnection(function(err, connection) 
+            {
+                // Use the connection
+                connection.query(queryStr, function (error, results, fields) 
+                {
+                    // And done with the connection.
+                    connection.release();
+                    //if Error reject promise
+                    if (error) {
+                        reject("Account creation failed");   
+                    }
+                    else {
+                        callback(true);
+                    }
+    
+                    //Can end event now that call is finished    
+                    context.callbackWaitsForEmptyEventLoop = false;
+                });
+            });
+        }
+        catch(e)
+        {
+            //If server is down return null
+            reject("Database is down.");
+        }
+    });
+}
