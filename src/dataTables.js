a86a89ae47e1d4448edd3afc7ffe0e2bb8ac41ae
Chris <cdcody@asu.edu>
2019-03-22 23:41:38 -0700

    Add table props, use ternary op for datatables

--- src/dataTables.js
+++ src/dataTables.js
@@ -14,27 +14,29 @@ export default class DataTables extends React.Component {
     }
 
     fetchData = () => {
-        const url = "https://ich20wj3w4.execute-api.us-west-1.amazonaws.com/table";//TODO get/create AWS API
+        const url = "https://ich20wj3w4.execute-api.us-west-1.amazonaws.com/table";
         fetch(url)
         .then(data=>{return data.json()})
         .then(result => {
-            this.setState({fetchedData: result.Data});
+            this.setState({fetchedData: result});
             console.log(result);
         });
     }
 
     render() {
-        const {fetchedData} = this.state || {};
+        const {fetchedData} = this.state;
+        const {type} = this.props;
 
-        switch(this.props.type) {
-            case "patients":
-                return <PatientTable data={fetchedData.appointments}/>;
-            case "appointments":
-                return <AppointmentTable data={fetchedData.appointments}/>;
-            case "users":
-                return <UserTable data={fetchedData.users}/>;
-            default: 
-                return null;
+        if(!fetchedData) {
+            return null;
+        }
+
+            return (
+                <div>
+                    {type === "patients" ? <PatientTable data={fetchedData.patients}/> :
+                    type === "appointments" ? <AppointmentTable data={fetchedData.appointments}/> :
+                    <UserTable data={fetchedData.staff}/>}
+                </div>
+            );
         }
     }
-}
--- src/userTable.js
+++ src/userTable.js
@@ -3,8 +3,21 @@ import ReactTable from "react-table";
 
 export default class UserTable extends React.Component {
     render() {
-        return <ReactTable columns={columns} data={this.props.data}/>
+        return <ReactTable columns={columns} data={this.props.data} getTdProps={getTdProps}/>
     }
+}
+
+//function passed to react table that determines props such as click handlers
+const getTdProps = (state, rowInfo, column, instance) => {
+    return {
+        onClick: (e, handleOriginal) => {
+
+        // triggers default events like expanding SubComponents and pivots.
+        if (handleOriginal) {
+            handleOriginal();
+        }
+        }
+    };
 }
 
 
