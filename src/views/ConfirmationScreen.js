62f243dbd7c5df5c3806e87b78b5c45822bfac1c
mechris6 <mechris6@asu.edu>
2019-03-25 11:42:37 -0700

    Confirmation screen now resets to transporter screen on success

--- src/views/ConfirmationScreen.js
+++ src/views/ConfirmationScreen.js
@@ -1,114 +1,113 @@
 import React, { Component } from "react";
-import { FlatList, ActivityIndicator, Text, View, StyleSheet, AppRegistry, Button} from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, StyleSheet, AppRegistry, Button } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 
 //Export ConfirmationScreen so App.js can call it for navigation
-export default class ConfirmationScreen extends React.Component 
-{
-    state = {
-        id:null,
-        loc:null,
-        isLoading: null
-    }
-    
-    confirmationObj = {
-        id:null
-    }
-    
-   constructor(props)
-   {
-       //Get objects from previous view
-        super(props)
-        this.state.id = this.props.navigation.getParam('id');
-        this.state.loc = this.props.navigation.getParam('loc');
-        
-        this.confirmationObj.id = this.state.id;
-        
-        this.state.isLoading = true;
-        //Do the server post to get data
-        this.doPostConfirmation();
-    }
+export default class ConfirmationScreen extends React.Component {
+  state = {
+    id: null,
+    loc: null,
+    isLoading: null
+  }
 
-    doPostConfirmation()
-    {
-        return fetch('https://8svpahmpbc.execute-api.us-west-1.amazonaws.com/Test',
-            { method: 'POST',headers: 
-            { Accept: 'application/json','Content-Type': 'application/json',}
-            , body: JSON.stringify(this.confirmationObj)})
-          .then((response) => response.json())
-          .then((responseJson) => {
+  confirmationObj = {
+    id: null
+  }
 
-            this.setState({
+  constructor(props) {
+    //Get objects from previous view
+    super(props)
+    this.state.id = this.props.navigation.getParam('id');
+    this.state.loc = this.props.navigation.getParam('loc');
+
+    this.confirmationObj.id = this.state.id;
+
+    this.state.isLoading = true;
+    //Do the server post to get data
+    this.doPostConfirmation();
+  }
+
+  doPostConfirmation() {
+    return fetch('https://8svpahmpbc.execute-api.us-west-1.amazonaws.com/Test',
+      {
+        method: 'POST', headers:
+          { Accept: 'application/json', 'Content-Type': 'application/json', }
+        , body: JSON.stringify(this.confirmationObj)
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
           isLoading: false,
           dataSource: responseJson,
-        }, function(){
-            //Testing functions
+        }, function () {
+          //Testing functions
         });
       })
-      .catch((error) =>{ console.error(error);});
-    }
-    
-    doPostConfirmationTrue()
-    {      
-        this.confirmationObj.location = this.state.loc;
-        alert(JSON.stringify(this.confirmationObj));
-        return fetch('https://k634ch08g9.execute-api.us-west-1.amazonaws.com/test',
-            { method: 'POST',headers: 
-            { Accept: 'application/json','Content-Type': 'application/json',}
-            , body: JSON.stringify(this.confirmationObj)})
-          .then((response) => response.json())
-          .then((responseJson) => {
+      .catch((error) => { console.error(error); });
+  }
 
-            this.setState({
+  doPostConfirmationTrue() {
+    this.confirmationObj.location = this.state.loc;
+    alert(JSON.stringify(this.confirmationObj));
+    return fetch('https://k634ch08g9.execute-api.us-west-1.amazonaws.com/test',
+      {
+        method: 'POST', headers:
+          { Accept: 'application/json', 'Content-Type': 'application/json', }
+        , body: JSON.stringify(this.confirmationObj)
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
           isLoading: false,
           dataSource: responseJson,
-        }, function()
-        {   
+        }, function () {
             //Testing functions go here
-        });
-        })
-          .catch((error) =>{
-            console.error(error);
+            this.props.navigation.navigate("Transporter")
           });
-        
-        // Change screen after post        
-    } 
-  
-  render() 
-  {
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-           <ActivityIndicator/>
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Change screen after post
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
         </View>
       )
     }
-    
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>     
-            <View style={{flex: 1, alignItems: 'center'}}> 
-                <Text>{JSON.stringify(this.state.dataSource)}</Text>
-                <Button
-                  title="Yes"
-                  onPress={() => this.doPostConfirmationTrue()}
-                />
-            </View>            
-            <View style={{flex: 1, alignItems: 'center'}}>
-                <Button
-                  title="No"
-                  onPress={() => this.props.navigation.navigate('Scanner')}
-                />
-            </View>
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Text>{JSON.stringify(this.state.dataSource)}</Text>
+          <Button
+            title="Yes"
+            onPress={() => this.doPostConfirmationTrue()}
+          />
         </View>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Button
+            title="No"
+            onPress={() => this.props.navigation.navigate('Scanner')}
+          />
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 10
+    flex: 1,
+    paddingTop: 10
   },
   item: {
     padding: 10,
