2dedf1ba293ce5fcd8a535644883a400874ce4b3
Leirbag <lfajardo@asu.edu>
2019-04-21 00:36:25 -0700

    Added fields tht will be passed for the creation of an account (i.e. full name).

--- accounts/createAccount/index.js
+++ accounts/createAccount/index.js
@@ -1,70 +1,86 @@
-var mysql = require('mysql');//establish sql connection pool
+var mysql = require("mysql"); //establish sql connection pool
 
-var pool  = mysql.createPool({
-    host: "arrivaldatabase.cerlvveo6skh.us-east-2.rds.amazonaws.com",
-    user: 'arrival',
-    password: 'brbUh86hELkWf82',
-    port: "3306",
-    database: 'app',
-  });
-  
+var pool = mysql.createPool({
+  host: "arrivaldatabase.cerlvveo6skh.us-east-2.rds.amazonaws.com",
+  user: "arrival",
+  password: "brbUh86hELkWf82",
+  port: "3306",
+  database: "app"
+});
+
 exports.handler = async (event, context, callback) => {
-    //parse event info
-    let {userName, passwordHash, userType, salt, phoneNumber, phoneType} = event;
-    
-    //add quotes to string variables
-    userName = addQuotes(userName);
-    passwordHash = addQuotes(passwordHash);
-    userType = addQuotes(userType);
-    phoneNumber = addQuotes(phoneNumber);
-    
-    IOS = 0;
-    
-    console.log(phoneType);
-    
-    if(phoneType == "IOS")
-    {
-        IOS = 1;
-    }
+  //parse event info
+  let {
+    userName,
+    passwordHash,
+    userType,
+    salt,
+    phoneNumber,
+    phoneType,
+    fullname
+  } = event;
 
-  var queryStr = "Insert into app.account" + 
-    "(userName, passwordHash, userType, salt, phoneNumber, IOS) " +
-    "Values (" + userName + ", " + passwordHash + ", " + userType + ", " + salt
-    + ", " + phoneNumber + ", " + IOS + ")";
-    console.log(queryStr);
-  
-    context.callbackWaitsForEmptyEventLoop = false;
-    return new Promise(function(resolve, reject)
-    {
-        try{
-            pool.getConnection(function(err, connection) 
-            {
-                // Use the connection
-                connection.query(queryStr, function (error, results, fields) 
-                {
-                    // And done with the connection.
-                    connection.release();
-                    //if Error reject promise
-                    if (error) {
-                        reject("Account creation failed" + JSON.stringify(error));   
-                    }
-                    else {
-                        resolve(true);
-                    }
-    
-                    //Can end event now that call is finished    
-                    context.callbackWaitsForEmptyEventLoop = false;
-                });
-            });
-        }
-        catch(e)
-        {
-            //If server is down return null
-            reject("Database is down.");
-        }
-    });
-}
+  //add quotes to string variables
+  userName = addQuotes(userName);
+  passwordHash = addQuotes(passwordHash);
+  userType = addQuotes(userType);
+  phoneNumber = addQuotes(phoneNumber);
+  fullname = addQuotes(fullname);
+
+  IOS = 0;
+
+  console.log(phoneType);
+
+  if (phoneType == "IOS") {
+    IOS = 1;
+  }
+
+  var queryStr =
+    "Insert into app.account" +
+    "(userName, passwordHash, userType, salt, phoneNumber, fullname, IOS) " +
+    "Values (" +
+    userName +
+    ", " +
+    passwordHash +
+    ", " +
+    userType +
+    ", " +
+    salt +
+    ", " +
+    phoneNumber +
+    ", " +
+    fullname +
+    ", " +
+    IOS +
+    ")";
+  console.log(queryStr);
+
+  context.callbackWaitsForEmptyEventLoop = false;
+  return new Promise(function(resolve, reject) {
+    try {
+      pool.getConnection(function(err, connection) {
+        // Use the connection
+        connection.query(queryStr, function(error, results, fields) {
+          // And done with the connection.
+          connection.release();
+          //if Error reject promise
+          if (error) {
+            reject("Account creation failed" + JSON.stringify(error));
+          } else {
+            resolve(true);
+          }
+
+          //Can end event now that call is finished
+          context.callbackWaitsForEmptyEventLoop = false;
+        });
+      });
+    } catch (e) {
+      //If server is down return null
+      reject("Database is down.");
+    }
+  });
+};
 
 function addQuotes(val) {
-        return "'" + val + "'";
+  return "'" + val + "'";
 }
