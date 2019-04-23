d85ac5b28bdca66a4b58688e8c8029e61f65fd1b
lfajardo20 <lfajardo@asu.edu>
2019-04-23 11:15:14 -0700

    Got the account creation to work. Added some required fields (i.e. cannot leave some blank)

--- src/newForms.js
+++ src/newForms.js
@@ -4,36 +4,51 @@ import "react-datepicker/dist/react-datepicker.css";
 import Crypto from "crypto-js";
 
 export default class NewForms extends React.Component {
+  constructor() {
+    super();
+    this.submitUser = this.submitUser.bind(this);
+  }
   state = {
-    values: {
-      startDate: new Date()
-    }
+    username: "",
+    phoneNumber: "",
+    password: "",
+    userType: "",
+    name: "",
+    startDate: new Date(),
   };
 
   submitPatient = () => {};
 
-  submitUser = async() => {
-    console.log("Entered the submitUser!");
-    let { values } = this.state;
+  /*
+  *The function below has been inspired by Medium post title 
+  *How to handle forms with just React by everdimension
+  * link: https://medium.com/@everdimension/how-to-handle-forms-with-just-react-ac066c48bd4f
+  * Also, used a for loop created by them to facilitate the data being sent to
+  * our API.
+  * link: https://gist.github.com/everdimension/87228e9ebab82b84afcdc7794fde3bfd
+  */
+  submitUser(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = {};
+    const url = 'https://6m2i2xewv6.execute-api.us-west-1.amazonaws.com/beta';
+
+    for (let element of form.elements) {
+      if (element.tagName === 'BUTTON') { continue; }
+      data[element.name] = element.value;
+    } 
 
     let salt = Math.floor(Math.random() * 10000); //generates random number between 0-10,000 as a salt
-    let hash = Crypto.SHA256(values.password + "" + salt); //using a salted, hashed password to prepare for actual security
+    let hash = Crypto.SHA256(data.password.toString() + "" + salt.toString()); //using a salted, hashed password to prepare for actual security
 
-    let accInfo = {
-      salt: salt,
-      passwordHash: hash.toString(Crypto.enc.Hex), //convert to hexadecimal string
-      userName: values.username,
-      phoneNumber: values.phoneNumber,
-      userType: values.userType,
-      fullname: values.name
-    };
-    this.postAccount(accInfo);
-  };
+    data["salt"] = salt;
+    data["passwordHash"] = hash.toString();
+    delete data["password"];
 
-  postAccount = data => {
-    const url = "https://6m2i2xewv6.execute-api.us-west-1.amazonaws.com/beta";
-    return fetch(url, {
-      method: "POST",
+    console.log(JSON.stringify(data));
+
+    fetch(url, {
+      method: 'POST',
       mode: "cors",
       headers: {
         Accept: "application/json",
@@ -41,141 +56,135 @@ export default class NewForms extends React.Component {
       },
       body: JSON.stringify(data)
     })
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(JSON.stringify(responseJson));
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(JSON.stringify(responseJson));
+    })
+    .catch(error => {
+      console.log(error);
+    });
   };
 
   submitAppointment = () => {};
 
   render() {
     const { type } = this.props;
-    const { values } = this.state;
+    //const { username, phoneNumber, password, userType, name } = this.state;
 
     if (!type) return null; //don't render if previous fields not selected
 
-    if (type === "patients") {
-      return (
-        <>
-          <h3>Adding a new Patient:</h3>
-          <form>
-            Name
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, name: text } })
-              }
-              placeholder="Name"
-            />
-            DOB
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, DOB: text } })
-              }
-              placeholder="MM/DD/YYYY"
-            />
-            Sex
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, sex: text } })
-              }
-            />
-            <br />
-            <button onClick={this.submitPatient()}>Submit</button>
-          </form>
-        </>
-      );
-    }
+    // if (type === "patients") {
+    //   return (
+    //     <>
+    //       <h3>Adding a new Patient:</h3>
+    //       <form>
+    //         Name
+    //         <input
+    //           onChange={text =>
+    //             this.setState({ values: { ...values, name: text } })
+    //           }
+    //           placeholder="Name"
+    //         />
+    //         DOB
+    //         <input
+    //           onChange={text =>
+    //             this.setState({ values: { ...values, DOB: text } })
+    //           }
+    //           placeholder="MM/DD/YYYY"
+    //         />
+    //         Sex
+    //         <input
+    //           onChange={text =>
+    //             this.setState({ values: { ...values, sex: text } })
+    //           }
+    //         />
+    //         <br />
+    //         <button onClick={this.submitPatient()}>Submit</button>
+    //       </form>
+    //     </>
+    //   );
+    // }
 
     if (type === "users") {
       return (
         <>
           <h3>Adding a new User:</h3>
-          <form>
+          <form onSubmit={this.submitUser}>
             Name
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, name: text } })
-              }
+            <input type="text"
               placeholder="Name"
+              id="fullname"
+              name="fullname"
             />
-            <select>
-              <option
-                onChange={() =>
-                  this.setState({ values: { ...values, userType: "staff" } })
-                }
+            <select id="userType" name="userType">
+              <option value="staff"
+                id="userType"
+                name="userType"
               >
                 Staff
               </option>
-              <option
-                onChange={() =>
-                  this.setState({
-                    values: { ...values, userType: "transporter" }
-                  })
-                }
+              <option value="transporter"
+                id="userType"
+                name="userType"
               >
                 Transporter
               </option>
             </select>
             Phone Number
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, phoneNumber: text } })
-              }
+            <input type="text"
               placeholder="123-456-7890"
+              id="phoneNumber"
+              name="phoneNumber"
             />
             Username
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, username: text } })
-              }
+            <input type="text"
+              id="userName"
+              name="userName"
+              required
             />
-            Initial Password(Defaults to 'Password)
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, password: text } })
-              }
+            Initial Password(Defaults to 'Password')
+            <input type="text"
+              id="password"
+              name="password"
+              required
             />
             <br />
-            <button type="submit" onClick={ () => this.submitUser()}>Submit</button>
+            <button>Submit</button>
           </form>
         </>
       );
     }
 
-    if (type === "appointments") {
-      return (
-        <>
-          <h3>Adding a new Appointment:</h3>
-          <form>
-            Date
-            <DatePicker
-              onChange={date =>
-                this.setState({ values: { ...values, startDate: date } })
-              }
-              selected={values.startDate}
-            />
-            Time
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, time: text } })
-              }
-              placeholder="24:00"
-            />
-            Location
-            <input
-              onChange={text =>
-                this.setState({ values: { ...values, location: text } })
-              }
-            />
-            <br />
-            <button onClick={this.submitAppointment()}>Submit</button>
-          </form>
-        </>
-      );
-    }
+    // if (type === "appointments") {
+    //   return (
+    //     <>
+    //       <h3>Adding a new Appointment:</h3>
+    //       <form>
+    //         Date
+    //         <DatePicker
+    //           onChange={date =>
+    //             this.setState({ values: { ...values, startDate: date } })
+    //           }
+    //           selected={values.startDate}
+    //         />
+    //         Time
+    //         <input
+    //           onChange={text =>
+    //             this.setState({ values: { ...values, time: text } })
+    //           }
+    //           placeholder="24:00"
+    //         />
+    //         Location
+    //         <input
+    //           onChange={text =>
+    //             this.setState({ values: { ...values, location: text } })
+    //           }
+    //         />
+    //         <br />
+    //         <button onClick={this.submitAppointment()}>Submit</button>
+    //       </form>
+    //     </>
+    //   );
+    // }
   }
 }
